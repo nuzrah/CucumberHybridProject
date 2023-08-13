2a1bee38dbdd7f598bf4f6982d68468fd5b2d256
nuzrah <141764710+nuzrah@users.noreply.github.com>
2023-08-14 00:09:04 +0530

    Update CommonUtils.java

--- src/main/java/utils/CommonUtils.java
+++ src/main/java/utils/CommonUtils.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class CommonUtils {
 
     public static final int IMPLICIT_WAIT_TIME=10;
-    public static final int PAGE_LOAD_TIME=25;
+    public static final int PAGE_LOAD_TIME=180;
     public static final int EXPLICIT_WAIT_BASIC_TIME=30;
 
     public String generateTimeStamp() {
